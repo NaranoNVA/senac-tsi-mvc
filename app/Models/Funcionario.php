e92c1cc6840280f025b2f511c08c479563325598
Nicolas Alberto <darkathnva@gmail.com>
2021-02-26 22:34:59 -0300

    Minha primeira model no Laravel

---
+++ app/Models/Funcionario.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Funcionario extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'nome',
+        'endereco',
+        'email',
+        'telefone'];
+
+    /*
+    É possivel mudar a chave primária assim
+    protected $primaryKey = 'nome_da_pk';
+
+    Se não quiser que seja auto_increment
+    public $increment = false;
+
+    Para definir o tipo
+    protected $keyType = 'string';
+
+    Tirar os campos timestamps
+    public $timestamps = false;
+    */
+
+    protected $table = 'Funcionario';
+}
